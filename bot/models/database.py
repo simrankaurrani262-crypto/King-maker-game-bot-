5b9de741bcb81279d1e1e27240d235f45c4e4da4
simrankaurrani262-crypto <simrankaurrani262@gmail.com>
2026-04-20 08:08:30 +0530

    Add files via upload

--- bot/models/database.py
+++ bot/models/database.py
@@ -14,7 +14,7 @@ engine = create_engine(
     echo=False
 )
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine, expire_on_commit=False)
 
 Base = declarative_base()
 
